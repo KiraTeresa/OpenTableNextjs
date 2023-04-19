537bc86df1529c4687d185179516ec6a671db5d9
KiraTeresa <kira.soba@gmx.de>
2023-04-19 17:19:46 +0200

    Fetching the restaurants menu

--- src/app/restaurant/[slug]/components/MenuCard.tsx
+++ src/app/restaurant/[slug]/components/MenuCard.tsx
@@ -1,11 +1,15 @@
-export default function MenuCard(){
+import {Item} from "@prisma/client";
+
+export default function MenuCard({item}: {item: Item}){
+    const {name, description, price} = item
+
     return (
         <div className=" border rounded p-3 w-[49%] mb-3">
-            <h3 className="font-bold text-lg">Surf and Turf</h3>
+            <h3 className="font-bold text-lg">{name}</h3>
             <p className="font-light mt-1 text-sm">
-                A well done steak with lobster and rice
+                {description}
             </p>
-            <p className="mt-7">$80.00</p>
+            <p className="mt-7">{price}</p>
         </div>
     )
 }
--- src/app/restaurant/[slug]/menu/page.tsx
+++ src/app/restaurant/[slug]/menu/page.tsx
@@ -1,12 +1,34 @@
 import Header from "@/app/restaurant/[slug]/components/Header";
 import RestaurantNavBar from "@/app/restaurant/[slug]/components/RestaurantNavBar";
 import Menu from "@/app/restaurant/[slug]/components/Menu";
+import {PrismaClient} from "@prisma/client";
 
-export default function RestaurantMenu() {
+const prisma = new PrismaClient();
+const fetchRestaurantMenu = async (slug: string) => {
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            slug
+        },
+        select: {
+            items: true
+        }
+    })
+
+    if (!restaurant){
+        throw new Error
+    }
+
+    return restaurant.items
+}
+
+export default async function RestaurantMenu({params}: {params: { slug: string }}) {
+
+    const menu = await fetchRestaurantMenu(params.slug)
+
     return (
         <div className="bg-white w-[100%] rounded p-3 shadow">
-            <RestaurantNavBar/>
-            <Menu/>
+            <RestaurantNavBar slug={params.slug}/>
+            <Menu menu={menu}/>
         </div>
     )
 }
