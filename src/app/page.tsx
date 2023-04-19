d3d12e2142427a538a423e46bc0e0cb8bae2c009
KiraTeresa <kira.soba@gmx.de>
2023-04-19 16:02:28 +0200

    Fetching data in a Server Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
 import {Inter} from '@next/font/google'
 import Header from "@/app/components/Header";
 import RestaurantCard from "@/app/components/RestaurantCard";
+import {PrismaClient} from "@prisma/client";
+
+/** This is a server component
+ *  No http requests, fetching data directly from the server
+ */
 
 const inter = Inter({subsets: ['latin']})
+const prisma = new PrismaClient()
 
-export default function Home() {
+const fetchRestaurants = async () => {
+    const restaurants = await prisma.restaurant.findMany();
+    return restaurants
+}
+
+export default async function Home() {
+    const restaurants = await fetchRestaurants()
+    console.log({restaurants})
+
     return (
                 <main>
                     <Header/>
-                    {/* CARDS */}
                     <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
                         <RestaurantCard/>
                     </div>
-                    {/* CARDS */}
                 </main>
     )
 }
