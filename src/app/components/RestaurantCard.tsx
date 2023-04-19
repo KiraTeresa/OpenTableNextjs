6e777020a92252dae06c0e5155641accbcea9e17
KiraTeresa <kira.soba@gmx.de>
2023-04-19 16:29:58 +0200

    Rendering fetched data

--- src/app/components/RestaurantCard.tsx
+++ src/app/components/RestaurantCard.tsx
@@ -1,26 +1,34 @@
 import Link from "next/link";
+import {RestaurantCardType} from "@/app/page";
+import Price from "@/app/components/Price";
 
-export default function RestaurantCard() {
+interface Props {
+    restaurant: RestaurantCardType;
+}
+
+export default function RestaurantCard({restaurant}: Props) {
+    const {name, main_image, cuisine, slug, location, price} = restaurant
+
     return (
         <div
             className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer"
         >
-            <Link href={"/restaurant/milestones-grill"}>
+            <Link href={`/restaurant/${slug}`}>
                 <img
-                    src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
+                    src={main_image}
                     alt=""
                     className="w-full h-36"
                 />
                 <div className="p-1">
-                    <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
+                    <h3 className="font-bold text-2xl mb-2">{name}</h3>
                     <div className="flex items-start">
                         <div className="flex mb-2">*****</div>
                         <p className="ml-2">77 reviews</p>
                     </div>
                     <div className="flex text-reg font-light capitalize">
-                        <p className=" mr-3">Mexican</p>
-                        <p className="mr-3">$$$$</p>
-                        <p>Toronto</p>
+                        <p className=" mr-3">{cuisine.name}</p>
+                        <Price price={price}/>
+                        <p>{location.name}</p>
                     </div>
                     <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
                 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,37 @@
 import {Inter} from '@next/font/google'
 import Header from "@/app/components/Header";
 import RestaurantCard from "@/app/components/RestaurantCard";
-import {PrismaClient} from "@prisma/client";
+import {PrismaClient, Cuisine, Location, PRICE} from "@prisma/client";
 
 /** This is a server component
  *  No http requests, fetching data directly from the server
  */
 
+export interface RestaurantCardType {
+    id: number;
+    name: string;
+    main_image: string;
+    cuisine: Cuisine;
+    slug: string;
+    location: Location;
+    price: PRICE;
+}
+
 const inter = Inter({subsets: ['latin']})
 const prisma = new PrismaClient()
 
-const fetchRestaurants = async () => {
-    const restaurants = await prisma.restaurant.findMany();
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+    const restaurants = await prisma.restaurant.findMany({
+        select: {
+            id: true,
+            name: true,
+            main_image: true,
+            cuisine: true,
+            slug: true,
+            location: true,
+            price: true
+        }
+    });
     return restaurants
 }
 
@@ -23,7 +43,9 @@ export default async function Home() {
                 <main>
                     <Header/>
                     <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-                        <RestaurantCard/>
+                        {restaurants.map((restaurant) => {
+                            return <RestaurantCard restaurant={restaurant}/>
+                        })}
                     </div>
                 </main>
     )
