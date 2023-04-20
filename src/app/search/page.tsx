23fbd01c460ebec58a413b3b05bf1bbd3cd02828
KiraTeresa <kira.soba@gmx.de>
2023-04-20 07:03:28 +0200

    Populating data on search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,40 @@
 import Header from "@/app/search/components/Header";
 import SearchSideBar from "@/app/search/components/SearchSideBar";
 import RestaurantCard from "@/app/search/components/RestaurantCard";
+import {RestaurantCardType} from "@/app/page";
+import {PrismaClient} from "@prisma/client";
+import {useSearchParams} from "next/navigation";
 
-export default function Search() {
+const prisma = new PrismaClient()
+const fetchRestaurantsByCity = (city: string | undefined): Promise<RestaurantCardType[]> => {
+    const select = {
+        id: true,
+        name: true,
+        main_image: true,
+        cuisine: true,
+        slug: true,
+        location: true,
+        price: true
+    }
+    if (!city) {
+        return prisma.restaurant.findMany({select})
+    }
+
+    return prisma.restaurant.findMany({
+        where: {
+            location: {
+                name: {
+                    equals: city.toLowerCase()
+                },
+            },
+        },
+        select,
+    });
+}
+
+export default async function Search({searchParams}: { searchParams: { city: string } }) {
+    const restaurants = await fetchRestaurantsByCity(searchParams.city)
+
     return (
         <>
             <Header/>
@@ -10,7 +42,15 @@ export default function Search() {
                 <SearchSideBar
                 />
                 <div className="w-5/6">
-                    <RestaurantCard/>
+                    {restaurants.length ?
+                        (
+                            <>
+                                {restaurants.map(restaurant => {
+                                    return <RestaurantCard key={restaurant.id} restaurant={restaurant}/>
+                                })}
+                            </>
+                        )
+                        : <p>Sorry, we found no restaurants in this area.</p>}
                 </div>
             </div>
         </>
