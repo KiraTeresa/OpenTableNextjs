d1bbfc118debe21a0ae304ed2cf7c4a1990da888
KiraTeresa <kira.soba@gmx.de>
2023-04-24 10:34:03 +0200

    adding a loading state to root

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+import Header from "@/app/components/Header";
+
+export default function Loading() {
+    return (
+        <main>
+            <Header/>
+            <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(num => (
+                    <div key={num}
+                         className="animate-pulse bg-slate-200 w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
+                    </div>
+                    ))}
+            </div>
+        </main>
+    )
+}
