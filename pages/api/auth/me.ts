ce36bcdb99e2da8672c8a45e7a0b5097fefebe17
KiraTeresa <kira.soba@gmx.de>
2023-04-24 17:13:05 +0200

    adding middleware

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -6,25 +6,9 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
     const bearerToken = req.headers["authorization"] as string
-
-    if (!bearerToken) {
-        return res.status(401).json({errorMessage: "Unauthorized request"})
-    }
-
     const token = bearerToken.split(" ")[1]
-
-    if (!token) {
-        return res.status(401).json({errorMessage: "Unauthorized request"})
-    }
-
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET)
-
-    try {
-        await jose.jwtVerify(token, secret)
-    } catch (error) {
-        return res.status(401).json({errorMessage: "Unauthorized request"})
-    }
 
     const payload = jwt.decode(token) as { email: string }
     if (!payload.email) {
