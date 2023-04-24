b3c9fb9b1b0ac0e6f66a68b85b902746b7fd6456
KiraTeresa <kira.soba@gmx.de>
2023-04-24 10:47:31 +0200

    Handling error states

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -34,7 +34,7 @@ const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
     })
 
     if(!restaurant){
-        throw new Error();
+        throw new Error("Cannot find restaurant");
     }
 
     return restaurant
