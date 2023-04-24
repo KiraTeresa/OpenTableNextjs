6816399ecb9381befef458febb8c66caeb6de4f0
KiraTeresa <kira.soba@gmx.de>
2023-04-24 14:27:14 +0200

    adding basic input validation

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,8 +1,54 @@
 import {NextApiRequest, NextApiResponse} from "next"
+import validator from "validator";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     if (req.method === "POST"){
+        const {firstName, lastName, email, city, phone} = req.body
+        const errors: string[] = []
+
+        const validationSchema = [{
+            valid: validator.isLength(firstName,{
+                min: 1,
+                max: 20,
+            }),
+            errorMessage: "First name is invalid"
+        },
+            {
+                valid: validator.isLength(lastName,{
+                    min: 1,
+                    max: 30,
+                }),
+                errorMessage: "Last name is invalid"
+            },
+            {
+                valid: validator.isEmail(email),
+                errorMessage: "Email is invalid"
+            },
+            {
+                valid: validator.isMobilePhone(phone),
+                errorMessage: "Phone number is invalid"
+            },
+            {
+                valid: validator.isLength(city,{min: 1}),
+                errorMessage: "City is Invalid"
+            },
+            {
+                valid: validator.isStrongPassword(password),
+                errorMessage: "Password is too weak"
+            }
+        ]
+
+        validationSchema.forEach(check => {
+            if(!check.valid){
+                errors.push(check.errorMessage)
+            }
+        })
+
+        if(errors.length){
+            return res.status(400).json({errorMessage: errors[0]})
+        }
+
         res.status(200).json({hello: "there"})
     }
 
