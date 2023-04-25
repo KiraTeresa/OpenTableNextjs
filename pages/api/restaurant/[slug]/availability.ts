70d85fe9d38d40b59c324fd4c7f742f3fc5365c1
KiraTeresa <kira.soba@gmx.de>
2023-04-25 10:27:50 +0200

    building availability endpoint

---
+++ pages/api/restaurant/[slug]/availability.ts
@@ -0,0 +1,16 @@
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default async function handler(req: NextApiRequest, res:NextApiResponse){
+    const {slug, day, time, partySize} = req.query as {
+        slug: string;
+        day: string;
+        time: string;
+        partySize: string
+    }
+
+    if(!day || !time || !partySize){
+        return res.status(400).json({errorMessage: "Invalid data provided"})
+    }
+
+    return res.json({slug, day, time, partySize})
+}
