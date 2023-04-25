e432ae56f69d75536e4f7529a6207ff1db1a955d
KiraTeresa <kira.soba@gmx.de>
2023-04-25 15:44:02 +0200

    Extracting the table availability logic into it's own function

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,6 +1,6 @@
 import {NextApiRequest, NextApiResponse} from "next";
-import {times} from "../../../../data";
 import {PrismaClient} from "@prisma/client";
+import {findAvailableTables} from "../../../../services/restaurant/findAvailableTables";
 
 const prisma = new PrismaClient()
 
@@ -30,38 +30,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).json({errorMessage: "Invalid data provided"})
     }
 
-    const searchTimes = times.find(t => {
-        return t.time === time
-    })?.searchTimes
-
-    if (!searchTimes) {
-        return res.status(400).json({errorMessage: "Invalid data provided"})
-    }
-
-    const bookings = await prisma.booking.findMany({
-        where: {
-            booking_time: {
-                gte: new Date(`${day}T${searchTimes[0]}`),
-                lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`)
-            }
-        },
-        select: {
-            number_of_people: true,
-            booking_time: true,
-            tables: true
-        }
-    })
-
-    const bookingTablesObj: { [key: string]: { [key: number]: true } } = {}
-
-    bookings.forEach(booking => {
-        bookingTablesObj[booking.booking_time.toISOString()] = booking.tables.reduce((obj, table) => {
-            return {
-                ...obj, [table.table_id]: true
-            }
-        }, {})
-    })
-
     const restaurant = await prisma.restaurant.findUnique({
         where: {
             slug
@@ -77,40 +45,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).json({errorMessage: "Invalid data provided"})
     }
 
-    const tables = restaurant.tables
+    const searchTimesWithTables = await findAvailableTables({day, time, res, restaurant})
 
-    const searchTimesWithTables = searchTimes.map(searchTime => {
-        return {
-            date: new Date(`${day}T${searchTime}`),
-            time: searchTime,
-            tables
-        }
-    })
-
-    searchTimesWithTables.forEach(t => {
-        t.tables = t.tables.filter(table => {
-            if (bookingTablesObj[t.date.toISOString()]) {
-                if (bookingTablesObj[t.date.toISOString()][table.id]) {
-                    return false
-                }
-            }
-            return true
-        })
-    })
+    if(!searchTimesWithTables){
+        return res.status(400).json({errorMessage: "Invalid data provided"})
+    }
 
     const availabilities = searchTimesWithTables.map(t => {
         const sumSeats = t.tables.reduce((sum, table) => {
             return sum + table.seats
-        },0)
+        }, 0)
 
         return {
             time: t.time,
             available: sumSeats >= parseInt(partySize)
         }
     }).filter(availability => {
-        const timeIsAfterOpeningHours = new Date(`${day}T${availability.time}`)>= new Date(`${day}T${restaurant.open_time}`)
+        const timeIsAfterOpeningHours = new Date(`${day}T${availability.time}`) >= new Date(`${day}T${restaurant.open_time}`)
 
-        const timeIsBeforeClosingHours = new Date(`${day}T${availability.time}`)<= new Date(`${day}T${restaurant.close_time}`)
+        const timeIsBeforeClosingHours = new Date(`${day}T${availability.time}`) <= new Date(`${day}T${restaurant.close_time}`)
 
         return timeIsAfterOpeningHours && timeIsBeforeClosingHours
     })
