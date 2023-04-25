f2edc6cd8950b739fe0fa50d13a1ac8971fb423e
KiraTeresa <kira.soba@gmx.de>
2023-04-25 08:55:07 +0200

    rendering a date picker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import NavBar from "@/app/components/NavBar";
 import AuthContext from "@/app/context/AuthContext";
 import React from "react";
+import "react-datepicker/dist/react-datepicker.css"
 
 /*export const metadata = {
     title: 'Create Next App',
