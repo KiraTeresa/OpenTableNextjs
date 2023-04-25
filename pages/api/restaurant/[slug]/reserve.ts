f027c7163bdbfb4bfcb80dd456002001c1bc7906
KiraTeresa <kira.soba@gmx.de>
2023-04-25 15:59:29 +0200

    count the tables based on seats

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -4,8 +4,8 @@ import {findAvailableTables} from "../../../../services/restaurant/findAvailable
 
 const prisma = new PrismaClient
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse){
-    const {slug, day, time, partySize} = req.query as {slug: string, day: string, time: string, partySize: string}
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const {slug, day, time, partySize} = req.query as { slug: string, day: string, time: string, partySize: string }
 
     const restaurant = await prisma.restaurant.findUnique({
         where: {
@@ -22,23 +22,39 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).json({errorMessage: "Restaurant not found"})
     }
 
-    if(new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) || new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)){
+    if (new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) || new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)) {
         return res.status(400).json({errorMessage: "Restaurant is not open at that time"})
     }
 
     const searchTimesWithTables = await findAvailableTables({day, time, res, restaurant})
 
-    if(!searchTimesWithTables){
+    if (!searchTimesWithTables) {
         return res.status(400).json({errorMessage: "Invalid data provided"})
     }
 
-    const searchTimeWithTables = searchTimesWithTables.find((t)=> {
+    const searchTimeWithTables = searchTimesWithTables.find((t) => {
         return t.date.toISOString() === new Date(`${day}T${time}`).toISOString()
     })
 
-    if(!searchTimeWithTables){
+    if (!searchTimeWithTables) {
         return res.status(400).json({errorMessage: "No availability"})
     }
 
-    return res.json({searchTimeWithTables})
+    const tablesCount: {
+        2: number[];
+        4: number[]
+    } = {
+        2: [],
+        4: []
+    }
+
+    searchTimeWithTables.tables.forEach(table => {
+        if(table.seats === 2){
+            tablesCount[2].push(table.id)
+        } else {
+            tablesCount[4].push(table.id)
+        }
+    })
+
+    return res.json({tablesCount})
 }
