f79cc26397b3c9a987622bf858ffe5092fd2347c
KiraTeresa <kira.soba@gmx.de>
2023-04-25 11:31:30 +0200

    fetching the bookings

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,5 +1,8 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {times} from "../../../../data";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res:NextApiResponse){
     const {slug, day, time, partySize} = req.query as {
@@ -15,11 +18,27 @@ export default async function handler(req: NextApiRequest, res:NextApiResponse){
 
     const searchTimes = times.find(t => {
         return t.time === time
-    })
+    })?.searchTimes
 
     if(!searchTimes){
         return res.status(400).json({errorMessage: "Invalid data provided!"})
     }
 
-    return res.json({searchTimes})
+    const bookings = await prisma.booking.findMany({
+        where: {
+            booking_time: {
+                gte: new Date(`${day}T${searchTimes[0]}`),
+                lte: new Date(`${day}T${searchTimes[searchTimes.length-1]}`)
+            }
+        },
+        select: {
+            number_of_people: true,
+            booking_time: true,
+            tables: true
+        }
+    })
+
+    return res.json({searchTimes, bookings})
 }
+
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-01-01&time=20:00:00.000Z&partySize=4
