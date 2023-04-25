013017e175b1c0243ec4da6916cdbafe14fad04a
KiraTeresa <kira.soba@gmx.de>
2023-04-25 12:03:23 +0200

    Step 3: compressing the booking

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -4,7 +4,7 @@ import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-export default async function handler(req: NextApiRequest, res:NextApiResponse){
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const {slug, day, time, partySize} = req.query as {
         slug: string;
         day: string;
@@ -12,7 +12,7 @@ export default async function handler(req: NextApiRequest, res:NextApiResponse){
         partySize: string
     }
 
-    if(!day || !time || !partySize){
+    if (!day || !time || !partySize) {
         return res.status(400).json({errorMessage: "Invalid data provided"})
     }
 
@@ -20,7 +20,7 @@ export default async function handler(req: NextApiRequest, res:NextApiResponse){
         return t.time === time
     })?.searchTimes
 
-    if(!searchTimes){
+    if (!searchTimes) {
         return res.status(400).json({errorMessage: "Invalid data provided!"})
     }
 
@@ -28,7 +28,7 @@ export default async function handler(req: NextApiRequest, res:NextApiResponse){
         where: {
             booking_time: {
                 gte: new Date(`${day}T${searchTimes[0]}`),
-                lte: new Date(`${day}T${searchTimes[searchTimes.length-1]}`)
+                lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`)
             }
         },
         select: {
@@ -38,7 +38,17 @@ export default async function handler(req: NextApiRequest, res:NextApiResponse){
         }
     })
 
-    return res.json({searchTimes, bookings})
+    const bookingTablesObj: { [key: string]: { [key: number]: true } } = {}
+
+    bookings.forEach(booking => {
+        bookingTablesObj[booking.booking_time.toISOString()] = booking.tables.reduce((obj, table) => {
+            return {
+                ...obj, [table.table_id]: true
+            }
+        }, {})
+    })
+
+    return res.json({searchTimes, bookings,bookingTablesObj})
 }
 
 // http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-01-01&time=20:00:00.000Z&partySize=4
