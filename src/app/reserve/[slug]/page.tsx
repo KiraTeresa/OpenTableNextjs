05439a59e451004d7ba01f414e62eabb58a489d1
KiraTeresa <kira.soba@gmx.de>
2023-04-26 08:30:30 +0200

    Dynamically rendering data for the reserve header

--- src/app/reserve/[slug]/page.tsx
+++ src/app/reserve/[slug]/page.tsx
@@ -1,11 +1,30 @@
 import Header from "@/app/reserve/[slug]/components/Header";
 import Form from "@/app/reserve/[slug]/components/Form";
+import {PrismaClient} from "@prisma/client";
+import {notFound} from "next/navigation";
 
-export default function Reserve() {
+const prisma = new PrismaClient()
+
+const fetchRestaurantBySlug = async (slug: string) => {
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            slug
+        }
+    })
+
+    if(!restaurant){
+        notFound()
+    }
+
+    return restaurant
+}
+export default async function Reserve({params}: {params: {slug: string}}) {
+    const restaurant = await fetchRestaurantBySlug(params.slug)
+
     return (
         <div className="border-t h-screen">
             <div className="py-9 w-3/5 m-auto">
-                <Header/>
+                <Header image={restaurant.main_image} name={restaurant.name}/>
                 <Form/>
             </div>
         </div>
